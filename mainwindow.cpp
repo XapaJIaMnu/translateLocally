bea4017c28983c1029d6648af14ee495e58fbb8e
Nikolay Bogoychev <nheart@gmail.com>
2021-04-13 15:02:03 +0100

    initial commit

---
+++ mainwindow.cpp
@@ -0,0 +1,20 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_translateButton_clicked()
+{
+    ui->outputBox->setText(ui->inputBox->toPlainText());
+}
