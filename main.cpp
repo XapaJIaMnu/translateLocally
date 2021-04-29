2ac89b23242355f3c16d2512d73f18a04b0696d8
Nikolay Bogoychev <nheart@gmail.com>
2021-04-29 23:05:12 +0100

    Print version and compile on 16.04

--- main.cpp
+++ main.cpp
@@ -1,11 +1,23 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QCommandLineParser>
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
+    QApplication translateLocally(argc, argv);
+    QCoreApplication::setApplicationName("translateLocally");
+    QCoreApplication::setApplicationVersion("0.02");
+
+    // Command line parsing
+    QCommandLineParser parser;
+    parser.setApplicationDescription("A secure translation service that performs translations for you locally, on your own machine.");
+    parser.addHelpOption();
+    parser.addVersionOption();
+    parser.process(translateLocally);
+
+    // Launch application
     MainWindow w;
     w.show();
-    return a.exec();
+    return translateLocally.exec();
 }
