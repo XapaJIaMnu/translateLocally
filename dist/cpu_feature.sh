44a1148ee826abf2d7de453817a6d0eef55fad71
Nikolay Bogoychev <nheart@gmail.com>
2021-05-26 23:08:52 +0000

    Output the arch of the build in the packages

---
+++ dist/cpu_feature.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+# Find out the OS
+unameOut="$(uname -s)"
+case "${unameOut}" in
+	Linux*)     machine=Linux;;
+	Darwin*)    machine=Mac;;
+	CYGWIN*)    machine=Cygwin;;
+	MINGW*)     machine=MinGw;;
+	*)          machine="UNKNOWN:${unameOut}"
+esac
+
+if [ "$machine" = "Linux" ]; then
+	if grep -q avx512 /proc/cpuinfo; then
+		echo "avx512"
+	elif grep -q avx2 /proc/cpuinfo; then
+		echo "avx2"
+	elif grep -q avx /proc/cpuinfo; then
+		echo "avx"
+	elif grep -q ssse3 /proc/cpuinfo; then
+		echo "ssse3"
+	else
+		echo "Really really old system"
+	fi
+elif [ "$machine" = "Mac" ]; then
+	if sysctl -a | grep cpu.features | grep -q AVX512; then
+		echo "avx512"
+	elif sysctl -a | grep cpu.features | grep -q AVX2; then
+		echo "avx2"
+	elif sysctl -a | grep cpu.features | grep -q AVX; then
+		echo "avx"
+	elif sysctl -a | grep cpu.features | grep -q SSSE3; then
+		echo "ssse3"
+	else
+		echo "Really really old machine."
+	fi
+else
+	echo "Unsupported platform"
+fi
