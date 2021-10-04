01c14bbacdc31831678beeff767e5c08e0883eb2
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2021-10-04 08:53:39 +0200

    Possibly fix missing meta type issue @XapaJIaMnu reported with Qt5/Linux

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "mainwindow.h"
 #include "version.h"
 #include "3rd_party/bergamot-translator/3rd_party/marian-dev/src/marian.h"
+#include "Translation.h"
 
 #include <QApplication>
 #include <QCommandLineParser>
@@ -9,6 +10,9 @@ int main(int argc, char *argv[])
 {
     // Set marian to throw exceptions instead of std::abort()
     marian::setThrowExceptionOnAbort(true);
+
+    // Register types so they can be used with Qt's signal/slots.
+    qRegisterMetaType<Translation>("Translation");
 
     QApplication translateLocally(argc, argv);
     QCoreApplication::setApplicationName("translateLocally");
