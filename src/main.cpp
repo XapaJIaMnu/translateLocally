75ad182a0be8ca9fae1266b370003161954b5b2b
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2021-10-14 12:49:33 +0200

    Fix for Qt5

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,7 @@ int main(int argc, char *argv[])
 
     // Register types so they can be used with Qt's signal/slots.
     qRegisterMetaType<Translation>("Translation");
+    qRegisterMetaType<QVector<WordAlignment>>("QVector<WordAlignment>");
 
     QApplication translateLocally(argc, argv);
     QCoreApplication::setApplicationName("translateLocally");
