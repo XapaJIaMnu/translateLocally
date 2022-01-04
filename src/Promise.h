bb87bb65ce1b08359f652984923a48228cdededa
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2022-01-04 13:55:44 +0100

    Bit more "why" for Promise

--- src/Promise.h
+++ src/Promise.h
@@ -3,10 +3,13 @@
 #include <QSemaphore>
 
 // Simple cancelable(!) promise because the machinery to do this
-// using std::future is still experimental.
+// using std::future is still experimental. Also, QFuture would be
+// an option, except that it requires you to call finish() after
+// cancelling (and explicitly forbids finishing multiple times)
+// which makes using it in an not-necessarily-called callback
+// hard.
 template<typename T>
 struct Promise {
-    QMutex lock_;
     QSemaphore semaphore_;
 
     T value;
@@ -22,6 +25,10 @@ struct Promise {
         semaphore_.release();
     }
 
+    // Call wait() before accessing cancelled or value. If wait returns true,
+    // the future was fulfilled. wait() blocks till the future was either
+    // fulfilled or cancelled. (If fulfill() and cancel() were called at the
+    // same time the output is undetermined.)
     bool wait() {
         semaphore_.acquire();
         return !cancelled;
